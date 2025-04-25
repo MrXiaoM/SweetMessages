424a84a684d6dce5b9e46b14db337c177ef9dde7
人間工作 <coolxiaom95@gmail.com>
2025-04-26 03:01:53 +0800

    upgrade dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,8 +1,3 @@
-import java.net.HttpURLConnection
-import java.net.URI
-import java.net.URL
-import java.util.*
-
 plugins {
     java
     `maven-publish`
@@ -16,14 +11,6 @@ val shadowGroup = "top.mrxiaom.sweet.messages.libs"
 repositories {
     mavenLocal()
     mavenCentral()
-    if (Locale.getDefault().country == "CN") runCatching {
-        val url = "https://maven.fastmirror.net/repositories/minecraft/"
-        val conn = URI.create(url).toURL().openConnection().apply { connect() } as HttpURLConnection
-        if (conn.responseCode == 200) maven(url)
-        else {
-            println("镜像仓库错误 (${conn.responseCode} ${conn.responseMessage})，不使用镜像")
-        }
-    }
     maven("https://repo.codemc.io/repository/maven-public/")
     maven("https://hub.spigotmc.org/nexus/content/repositories/snapshots/")
     maven("https://repo.extendedclip.com/content/repositories/placeholderapi/")
@@ -41,11 +28,11 @@ dependencies {
 
     compileOnly("me.clip:placeholderapi:2.11.6")
 
-    implementation("net.kyori:adventure-api:4.17.0")
+    implementation("net.kyori:adventure-api:4.20.0")
     implementation("net.kyori:adventure-platform-bukkit:4.3.4")
-    implementation("net.kyori:adventure-text-minimessage:4.17.0")
+    implementation("net.kyori:adventure-text-minimessage:4.20.0")
     implementation("org.jetbrains:annotations:24.0.0")
-    implementation("top.mrxiaom:PluginBase:1.3.7")
+    implementation("top.mrxiaom:PluginBase:1.4.0")
     implementation("com.github.technicallycoded:FoliaLib:0.4.4")
     implementation(project(":nms"))
     for (proj in project.project(":nms").subprojects) {
