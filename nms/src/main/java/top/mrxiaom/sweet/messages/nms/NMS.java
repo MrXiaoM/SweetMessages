7937aa0887ded213b15c47aa5ca6c1fd30d5a50d
MrXiaoM <coolxiaom95@gmail.com>
2025-07-10 09:38:43 +0800

    support 1.21.6/1.21.7

--- nms/src/main/java/top/mrxiaom/sweet/messages/nms/NMS.java
+++ nms/src/main/java/top/mrxiaom/sweet/messages/nms/NMS.java
@@ -24,6 +24,8 @@ public class NMS {
         put("1.21.3", "v1_21_R2");
         put("1.21.4", "v1_21_R3");
         put("1.21.5", "v1_21_R4");
+        put("1.21.6", "v1_21_R5");
+        put("1.21.7", "v1_21_R5");
     }};
 
     @SuppressWarnings("UnusedReturnValue")
