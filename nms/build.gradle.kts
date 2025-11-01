50c0ddb176e701877fe76a3047f8ebfb4ea0dc1d
MrXiaoM <coolxiaom95@gmail.com>
2025-11-01 16:26:09 +0800

    Update build.gradle.kts

--- nms/build.gradle.kts
+++ nms/build.gradle.kts
@@ -17,7 +17,7 @@ setupJava(8)
 @Suppress("VulnerableLibrariesLocal")
 dependencies {
     "compileOnly"("org.spigotmc:spigot-api:1.20-R0.1-SNAPSHOT")
-    "implementation"("org.jetbrains:annotations:24.0.0")
+    "compileOnly"("org.jetbrains:annotations:24.0.0")
     for (proj in subprojects) {
         if (!proj.name.startsWith("v")) {
             "implementation"(proj)
