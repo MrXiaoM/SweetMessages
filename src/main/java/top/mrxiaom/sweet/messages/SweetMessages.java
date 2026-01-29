10f59c6b8106173670642cf4e86bcad07245954c
MrXiaoM <coolxiaom95@gmail.com>
2026-01-29 13:32:30 +0800

    Update SweetMessages.java

--- src/main/java/top/mrxiaom/sweet/messages/SweetMessages.java
+++ src/main/java/top/mrxiaom/sweet/messages/SweetMessages.java
@@ -1,8 +1,10 @@
 package top.mrxiaom.sweet.messages;
 
 import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.configuration.file.YamlConfiguration;
 import top.mrxiaom.pluginbase.resolver.DefaultLibraryResolver;
 import top.mrxiaom.pluginbase.utils.ClassLoaderWrapper;
+import top.mrxiaom.pluginbase.utils.ConfigUtils;
 import top.mrxiaom.pluginbase.utils.Util;
 import top.mrxiaom.pluginbase.utils.scheduler.FoliaLibScheduler;
 import top.mrxiaom.sweet.messages.api.IBossBarFactory;
@@ -39,6 +41,10 @@ public class SweetMessages extends BukkitPlugin {
                 : new File(this.getDataFolder(), "libraries");
         DefaultLibraryResolver resolver = new DefaultLibraryResolver(getLogger(), librariesDir);
 
+        YamlConfiguration overrideLibraries = ConfigUtils.load(resolve("./.override-libraries.yml"));
+        for (String key : overrideLibraries.getKeys(false)) {
+            resolver.getStartsReplacer().put(key, overrideLibraries.getString(key));
+        }
         resolver.addResolvedLibrary(BuildConstants.RESOLVED_LIBRARIES);
 
         List<URL> libraries = resolver.doResolve();
