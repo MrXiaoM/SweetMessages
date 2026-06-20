da7be265eb69e0973a3954498a007f155ce87476
MrXiaoM <coolxiaom95@gmail.com>
2026-06-20 19:54:48 +0800

    simplify unobfuscated nms call

--- nms/mojmap_26_2/src/main/java/top/mrxiaom/sweet/messages/nms/BossBar_26_2.java
+++ nms/mojmap_26_2/src/main/java/top/mrxiaom/sweet/messages/nms/BossBar_26_2.java
@@ -9,22 +9,23 @@ import net.minecraft.server.level.ServerBossEvent;
 import org.bukkit.boss.BossBar;
 import org.bukkit.craftbukkit.boss.CraftBossBar;
 
-import java.util.Optional;
-
 public class BossBar_26_2 implements IBossBar {
     GsonComponentSerializer serializer = GsonComponentSerializer.gson();
     @Override
     public void setTitle(BossBar bossBar, Component component) {
         ServerBossEvent nms = ((CraftBossBar) bossBar).getHandle();
         JsonElement json = serializer.serializeToTree(component);
-        Optional<net.minecraft.network.chat.Component> result = ComponentSerialization.CODEC.parse(JsonOps.INSTANCE, json).result();
-        result.ifPresent(nms::setName);
+        ComponentSerialization.CODEC.parse(JsonOps.INSTANCE, json)
+                .result()
+                .ifPresent(nms::setName);
     }
 
     public Component getTitle(BossBar bossBar) {
         ServerBossEvent nms = ((CraftBossBar) bossBar).getHandle();
         if (nms == null) return null;
-        Optional<JsonElement> result = ComponentSerialization.CODEC.encodeStart(JsonOps.INSTANCE, nms.getName()).result();
-        return result.map(serializer::deserializeFromTree).orElse(null);
+        return ComponentSerialization.CODEC.encodeStart(JsonOps.INSTANCE, nms.getName())
+                .result()
+                .map(serializer::deserializeFromTree)
+                .orElse(null);
     }
 }
